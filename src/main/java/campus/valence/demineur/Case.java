f103d7b4918a203dc5a81956e39e68c5541744e6
K-ply <karine.pauly@yahoo.fr>
2022-05-24 12:00:04 +0200

    tests validés 1 2 et 3

--- src/main/java/campus/valence/demineur/Case.java
+++ src/main/java/campus/valence/demineur/Case.java
@@ -1,6 +1,37 @@
 package campus.valence.demineur;
 
+import java.util.Objects;
+
 public class Case {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Case aCase = (Case) o;
+        return x == aCase.x && y == aCase.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     static class BombCase extends Case {
 
--- src/main/java/campus/valence/demineur/Demineur.java
+++ src/main/java/campus/valence/demineur/Demineur.java
@@ -1,13 +1,31 @@
 package campus.valence.demineur;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class Demineur {
+    private Case[][] board;
+
 
     public static Demineur create(int bombCount, int boardSize) {
+
         Case[][] board = new Case[boardSize][boardSize];
-        return null;
+        Set<Case.BombCase> bombes = new HashSet<>();
+
+        while (bombes.size() < bombCount) {
+            int x = (int) (Math.random() * boardSize);
+            int y = (int) (Math.random() * boardSize);
+            System.out.println("test" + x);
+            System.out.println("test" + y);
+            bombes.add(new Case.BombCase(x, y));
+        }
+        for (Case.BombCase bomb : bombes) {
+            board[bomb.getX()][bomb.getY()] = bomb;
+        }
+        return new Demineur(board);
     }
 
-    private Case[][] board;
 
     protected Demineur(Case[][] board) {
         this.board = board;
@@ -22,6 +40,23 @@ public class Demineur {
     }
 
     public int countBombsAround(int x, int y) {
-        throw new RuntimeException("To be implemented");
+        try {
+            int count = 0;
+
+            for (x = 0; x < board.length; x++) {
+                for (y = 0; y < board.length; y++) {
+                  if(board[x-1]== null){
+                      count = count+1;
+                  }
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("To be implemented");
+
+        }
+
+
+        return x;
     }
 }
